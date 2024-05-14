36c8849eeac3ae5bbd0a610c5c7b606d56818c0c
sotaro39 <avakonbu@yahoo.co.jp>
2024-05-14 14:54:24 +0900

    rest of css

--- FEtest/resources/views/layouts/app.blade.php
+++ FEtest/resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <header>
         <div class="header-container">
             <div class="header-contents">
-                <div class="icons">
+                <div class="header-icons">
                     <div>基本情報技術者 試験情報＆徹底解説 -新制度に完全対応-</div>
                     <div><img src="{{ asset('storage/d-origin.jpg') }}" width="500" height="70"></div>
                 </div>
@@ -25,7 +25,7 @@
             </div>
         </div>
 
-        <ul class="header-menu">
+        <ul class="nav-menu">
             <li><a href="#">過去問解説</a>
                 <ul>
                     <li><a href="#">令和３年</a></li>
@@ -43,11 +43,14 @@
             <li><a href="{{ route('login.index') }}">Login</a></li>
         </ul>
 
+
+
+
     </header>
     <main class="container mt-10">
         <div class="row">
-            <div class="col-md-8">
-                <article>
+            <div class="col-md-7">
+                <article class="main-contents">
                     <div>
                         @yield('content')
                     </div>
@@ -55,16 +58,93 @@
             </div>
             <div class="col-md-4">
                 <div class="side">
-                    <div>aaaaaaaaaaaaaaaaaaa</div>
-                    <div>aaaaaaaaaaaaaaaaaaa</div>
-                    <div>aaaaaaaaaaaaaaaaaaa</div>
-                    <div>aaaaaaaaaaaaaaaaaaa</div>
-                    <div>aaaaaaaaaaaaaaaaaaa</div>
-                    <div>aaaaaaaaaaaaaaaaaaa</div>
-                    <div>aaaaaaaaaaaaaaaaaaa</div>
+                    <a href="#"><img class="side-img"></a>
+                </div>
+                <div class="side">
+                    <div class="ribon">Welcome to My Blog!</div>
+                    <div class="container text-center side-tab">
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">基本情報技術者とは</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">基本情報技術者メリット</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">試験範囲</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">出題範囲</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">試験の形式と合格基準</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">基本情報技術者の難易度</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">科目A試験の免除制度</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">効果的な勉強方法</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">おすすめテキスト問題集</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">通信講座の紹介</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">よくある質問(FAQ)</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">統計情報</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="side">
+                    <a href="#"><img class="side-img"></a>
                 </div>
 
+                <div class="side">
+                    <div class="ribon">Welcome to My Blog!</div>
+                    <div class="container text-center">
+                        <div class="row">
+                            <div class="col">
+                                令和５年度
+                            </div>
+                            <div class="col">
+                                新制度サンプル問題
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="side">
+                    <div class="ribon">基本情報技術者過去問大</div>
+                    <div class="container text-center">
+                        <div class="row">
+                            <div class="col">
+                                令和５年度
+                            </div>
+                            <div class="col">
+                                新制度サンプル問題
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </div>
     </main>
 </body>
