181ce8157435ed1c07281485d55436df6fa680d3
sotaro39 <avakonbu@yahoo.co.jp>
2024-05-20 14:36:50 +0900

    css

--- FEtest/resources/views/layouts/app.blade.php
+++ FEtest/resources/views/layouts/app.blade.php
@@ -121,11 +121,12 @@
                     <div class="ribon">Welcome to My Blog!</div>
                     <div class="container text-center">
                         <div class="row">
+
                             <div class="col">
-                                令和５年度
+                                <a href="#">令和5年度</a>
                             </div>
                             <div class="col">
-                                新制度サンプル問題
+                                <a href="#">新制度サンプル問題</a>
                             </div>
                         </div>
                     </div>
@@ -135,13 +136,219 @@
                     <div class="container text-center">
                         <div class="row">
                             <div class="col">
-                                令和５年度
+                                <a href="#">令和4年度</a>
                             </div>
                             <div class="col">
-                                新制度サンプル問題
+                                <a href="#">令和3年度</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">令和2年度</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">令和元年秋期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成31年春期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成30年秋期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成30年春期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成29年秋期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成29年春期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成28年秋期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成28年春期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成27年秋期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成27年春期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成26年秋期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成26年春期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成25年秋期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成25年春期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成24年秋期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成24年春期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成23年秋期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成23年春期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成22年秋期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成22年春期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成21年秋期</a>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <a href="#">平成21年春期</a>
+                                </div>
+                                <div class="col">
+                                </div>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="side">
+                    <a href="#"><img class="side-img"></a>
+                </div>
+                <div class="side">
+                    <div class="ribon">基本情報技術者過去問大</div>
+                    <div class="container text-center">
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成20年秋期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成20年春期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成19年秋期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成19年春期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成18年秋期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成18年春期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成17年秋期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成17年春期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成16年秋期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成16年春期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成15年秋期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成15年春期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成14年秋期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成14年春期</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#">平成13年秋期</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">平成13年春期</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="side">
+                    <div class="ribon">Welcome to My Blog!</div>
+                    <div class="container text-center">
+                        <div class="row">
+
+                            <div class="col">
+                                <a href="#">模擬試験１</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">模擬試験２</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="side">
+                    <div class="ribon">Welcome to My Blog!</div>
+                    <div class="container text-center">
+                        <div class="row">
+
+                            <div class="col">
+                                <a href="#">テクノロジ系</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">マネジメント系</a>
+                            </div>
+                        </div>
+                        <div class="row">
+
+                            <div class="col">
+                                <a href="#">ストラテジ系</a>
+                            </div>
+                            <div class="col">
+                                <a href="#">午後問題分野別まとめ</a>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
